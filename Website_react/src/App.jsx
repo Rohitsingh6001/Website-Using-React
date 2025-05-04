7f5edd0eb5611ef707a1a07c8dd55e3c789ad51e
Rohitsingh6001 <Rohitsingh837000@gmail.com>
2025-05-04 15:50:27 +0530

    Ok completed

--- Website_react/src/App.jsx
+++ Website_react/src/App.jsx
@@ -5,6 +5,9 @@ import Program from "./Components/Programs/Program";
 import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
 import Campus from "./Components/Campus/Campus";
+import Testimonials from "./Components/Testimonils/Testimonials";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <>
@@ -16,6 +19,11 @@ function App() {
         <About/>
         <Title subTitle = 'Gallery' title = 'Campus Photos'/>
         <Campus/>
+        <Title subTitle = 'TESTIMONIALS' title = 'What Student Says'/>
+        <Testimonials/>
+        <Title subTitle = 'Contact Us' title = 'Get in Touch'/>
+        <Contact/>
+        <Footer/>
       </div>
     </>
   );
